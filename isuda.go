a19be2fb9bd987588446f442f15c3c8d27400832
山田雄太 <yamadayuuta@yamadayuutanoMacBook-Pro.local>
2018-06-09 16:20:39 +0900

    isutarを統合する

--- isuda.go
+++ isuda.go
@@ -31,7 +31,7 @@ const (
 )
 
 var (
-	isutarEndpoint string
+	//isutarEndpoint string
 	isupamEndpoint string
 
 	baseUrl *url.URL
@@ -73,9 +73,8 @@ func initializeHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := db.Exec(`DELETE FROM entry WHERE id > 7101`)
 	panicIf(err)
 
-	resp, err := http.Get(fmt.Sprintf("%s/initialize", isutarEndpoint))
+	_, err = db.Exec(`TRUNCATE star`)
 	panicIf(err)
-	defer resp.Body.Close()
 
 	re.JSON(w, http.StatusOK, map[string]string{"result": "ok"})
 }
@@ -337,18 +336,42 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 }
 
 func loadStars(keyword string) []*Star {
-	v := url.Values{}
-	v.Set("keyword", keyword)
-	resp, err := http.Get(fmt.Sprintf("%s/stars", isutarEndpoint) + "?" + v.Encode())
+	rows, _ := db.Query(`SELECT * FROM star WHERE keyword = ?`, keyword)
+
+	stars := make([]*Star, 0, 10)
+	for rows.Next() {
+		s := Star{}
+		err := rows.Scan(&s.ID, &s.Keyword, &s.UserName, &s.CreatedAt)
+		panicIf(err)
+		stars = append(stars, &s)
+	}
+	rows.Close()
+
+	return stars
+}
+
+func starsPostHandler(w http.ResponseWriter, r *http.Request) {
+	keyword := r.FormValue("keyword")
+
+	origin := os.Getenv("ISUDA_ORIGIN")
+	if origin == "" {
+		origin = "http://localhost:5000"
+	}
+	u, err := r.URL.Parse(fmt.Sprintf("%s/keyword/%s", origin, pathURIEscape(keyword)))
+	panicIf(err)
+	resp, err := http.Get(u.String())
 	panicIf(err)
 	defer resp.Body.Close()
-
-	var data struct {
-		Result []*Star `json:result`
+	if resp.StatusCode >= 400 {
+		notFound(w)
+		return
 	}
-	err = json.NewDecoder(resp.Body).Decode(&data)
+
+	user := r.FormValue("user")
+	_, err = db.Exec(`INSERT INTO star (keyword, user_name, created_at) VALUES (?, ?, NOW())`, keyword, user)
 	panicIf(err)
-	return data.Result
+
+	re.JSON(w, http.StatusOK, map[string]string{"result": "ok"})
 }
 
 func isSpamContents(content string) bool {
@@ -417,10 +440,10 @@ func main() {
 	db.Exec("SET SESSION sql_mode='TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY'")
 	db.Exec("SET NAMES utf8mb4")
 
-	isutarEndpoint = os.Getenv("ISUTAR_ORIGIN")
-	if isutarEndpoint == "" {
-		isutarEndpoint = "http://localhost:5001"
-	}
+	//isutarEndpoint = os.Getenv("ISUTAR_ORIGIN")
+	///if isutarEndpoint == "" {
+	//	isutarEndpoint = "http://localhost:5001"
+	//}
 	isupamEndpoint = os.Getenv("ISUPAM_ORIGIN")
 	if isupamEndpoint == "" {
 		isupamEndpoint = "http://localhost:5050"
@@ -469,6 +492,9 @@ func main() {
 	k := r.PathPrefix("/keyword/{keyword}").Subrouter()
 	k.Methods("GET").HandlerFunc(myHandler(keywordByKeywordHandler))
 	k.Methods("POST").HandlerFunc(myHandler(keywordByKeywordDeleteHandler))
+
+	s := r.PathPrefix("/stars").Subrouter()
+	s.Methods("POST").HandlerFunc(myHandler(starsPostHandler))
 
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 	log.Fatal(http.ListenAndServe(":5000", r))
